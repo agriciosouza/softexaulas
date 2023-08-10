64042d906bbd4739cf844c0f6e562555d0586fd7
Agricio Souza <99698837+agriciosouza@users.noreply.github.com>
2023-08-10 17:15:36 -0300

    Create Atividade 16.js

---
+++ Projetos Softex/01 - Lógica de Programação/Atividade 16.js
@@ -0,0 +1,18 @@
+// Criando as listas
+var nomes = ['Ana', 'João', 'Maria', 'Pedro', 'Luiza', 'Rafael', 'Carla', 'Lucas', 'Juliana', 'Fernando'];
+var idades = [25, 30, 22, 28, 24, 27, 29, 31, 26, 23];
+var coresFavoritas = ['azul', 'vermelho', 'verde', 'amarelo', 'roxo', 'laranja', 'rosa', 'preto', 'branco', 'marrom'];
+
+// Imprimindo as listas originais
+console.log('Nomes:', nomes);
+console.log('Idades:', idades);
+console.log('Cores Favoritas:', coresFavoritas);
+
+// Modificando uma cor e uma idade
+idades[2] = 23; // Modificando a idade da terceira pessoa
+coresFavoritas[4] = 'azul-marinho'; // Modificando a cor favorita da quinta pessoa
+
+// Imprimindo as listas modificadas
+console.log('Nomes:', nomes);
+console.log('Idades:', idades);
+console.log('Cores Favoritas:', coresFavoritas);
