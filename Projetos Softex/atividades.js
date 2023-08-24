49c87b743663e212ede51f8905487ce59f7c0545
Agricio Souza <99698837+agriciosouza@users.noreply.github.com>
2023-08-24 08:49:48 -0300

    Update atv_for_in_for_of.js

--- Projetos Softex/atividades.js
+++ Projetos Softex/atividades.js
@@ -1,8 +1,15 @@
+// 1 - Dado um array de números [3, 7, 2, 9, 5], crie um loop "for" que itere pelo array e exiba cada elemento no console.
+
 // const numeros [3, 7, 2, 9, 5,];
 // for (let i = 0; i < numeros.length; i++) {
 //     console.log(numeros[i]);
 //   }
   
+
+//Suponha que você tenha um objeto com valores numéricos: {a: 10, b: 20, c: 30, d: 40}. 
+//Utilize um laço "for...in" para iterar pelas chaves do objeto e calcular a soma de todos os valores.
+
+
 //   const objeto = {a: 10, b: 20, c: 30, d: 40};
 // let soma = 0;
 
@@ -13,6 +20,10 @@
 // }
 
 //console.log("A soma dos valores é:", soma);
+
+
+//Dado um array de números [12, 5, 8, 21, 16, 7], crie um loop "for" que itere pelo array, verificando se cada número é par. 
+//Se for par, adicione-o a um novo array. No final, exiba o novo array com os números pares
 
 // const numeros = [12, 5, 8, 21, 16, 7];
 // const numerosPares = [];
@@ -25,6 +36,9 @@
 
 // console.log(numerosPares);
 
+//Considere um objeto com informações de estudantes: const estudantes = { alice: 18, bob: 20, carol: 19, david: 21 }; 
+//Utilize um loop "for...in" para verificar se há um estudante com a idade de 19 anos. Se sim, exiba "Nome do estudante: idade" no console.
+
 // const estudantes = { alice: 18, bob: 20, carol: 19, david: 21 };
 
 // for (const nome in estudantes) {
@@ -32,6 +46,9 @@
 // console.log(`${nome}: ${estudantes[nome]} anos`);
 //    }
 // }
+
+//Imagine que você tem uma matriz bidimensional representando uma grade de pontos: const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ]; 
+//Utilize dois loops "for" aninhados para calcular a soma de todos os elementos da matriz e exibir o resultado.
 
 //const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ];
 //let soma = 0;
