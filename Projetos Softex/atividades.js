d696d0b3cc2c8b48df7801b35a8eaae81a1860bd
Agricio Souza <99698837+agriciosouza@users.noreply.github.com>
2023-08-24 08:38:11 -0300

    Create atividades.js

---
+++ Projetos Softex/atividades.js
@@ -0,0 +1,45 @@
+// const numeros [3, 7, 2, 9, 5,];
+// for (let i = 0; i < numeros.length; i++) {
+//     console.log(numeros[i]);
+//   }
+  
+//   const objeto = {a: 10, b: 20, c: 30, d: 40};
+// let soma = 0;
+
+// for (const chave in objeto) {
+//    if (objeto.hasOwnProperty(chave)) {
+//    soma += objeto[chave];
+// }
+// }
+
+//console.log("A soma dos valores é:", soma);
+
+// const numeros = [12, 5, 8, 21, 16, 7];
+// const numerosPares = [];
+
+// for (let i = 0; i < numeros.length; i++) {
+//   if (numeros[i] % 2 === 0) {
+//     numerosPares.push(numeros[i]);
+//   }
+// }
+
+// console.log(numerosPares);
+
+// const estudantes = { alice: 18, bob: 20, carol: 19, david: 21 };
+
+// for (const nome in estudantes) {
+// if (estudantes[nome] === 19) {
+// console.log(`${nome}: ${estudantes[nome]} anos`);
+//    }
+// }
+
+//const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ];
+//let soma = 0;
+
+// for (let i = 0; i < pontos.length; i++) {
+//   for (let j = 0; j < pontos[i].length; j++) {
+//     soma += pontos[i][j];
+//   }
+// }
+
+// console.log(`A soma de todos os elementos é: ${soma}`);
